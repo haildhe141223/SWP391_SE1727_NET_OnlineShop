2258dea908f86ea02e6eadf45bb324ca92d1c573
dathmhe141249 <dathmhe141249@fpt.edu.vn>
2023-09-25 00:11:51 +0700

    Update Repo, Service, ViewModel of Order Model

--- SWP391.OnlineShop.Service/Configs/AutoMapper/AutoMapperConfigs.cs
+++ SWP391.OnlineShop.Service/Configs/AutoMapper/AutoMapperConfigs.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using SWP391.OnlineShop.Core.Models.Entities;
+using SWP391.OnlineShop.ServiceModel.ViewModels.Cart;
 
 namespace SWP391.OnlineShop.Service.Configs.AutoMapper;
 
@@ -6,5 +8,7 @@ public class AutoMapperConfigs : Profile
 {
     public AutoMapperConfigs()
     {
+        //// Project
+        CreateMap<Order, OrderDetailViewModel>().ReverseMap();
     }
 }
