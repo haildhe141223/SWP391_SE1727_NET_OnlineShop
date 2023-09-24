06b94009cfd97d44353be89c7fdd511df4d0210c
PhuongNLHE141219 <phuongnlhe141219@fpt.edu.vn>
2023-09-24 23:58:44 +0700

    OSP. Add Service Logic for Post

--- SWP391.OnlineShop.Service/Configs/AutoMapper/AutoMapperConfigs.cs
+++ SWP391.OnlineShop.Service/Configs/AutoMapper/AutoMapperConfigs.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using SWP391.OnlineShop.Core.Models.Entities;
+using SWP391.OnlineShop.ServiceModel.ViewModels.Products;
 
 namespace SWP391.OnlineShop.Service.Configs.AutoMapper;
 
@@ -6,5 +8,8 @@ public class AutoMapperConfigs : Profile
 {
     public AutoMapperConfigs()
     {
+        //// Product
+        CreateMap<Product, ProductViewModel>().ReverseMap();
+        CreateMap<Post, PostViewModel>().ReverseMap();
     }
 }
