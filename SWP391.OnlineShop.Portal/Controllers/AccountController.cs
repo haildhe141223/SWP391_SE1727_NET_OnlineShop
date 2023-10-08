014da4ad87a09a914dbb7171023ac2c5458d0a69
haildhe141223 <haildhe141223@fpt.edu.vn>
2023-10-09 00:59:36 +0700

    OSP: update Admin UI

--- SWP391.OnlineShop.Portal/Controllers/AccountController.cs
+++ SWP391.OnlineShop.Portal/Controllers/AccountController.cs
@@ -1,12 +1,264 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack;
+using SWP391.OnlineShop.Core.Models.Identities;
+using SWP391.OnlineShop.ServiceInterface.Loggers;
+using SWP391.OnlineShop.ServiceModel.ServiceModels;
+using SWP391.OnlineShop.ServiceModel.ViewModels.Accounts;
+using System.Security.Claims;
 
 namespace SWP391.OnlineShop.Portal.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILoggerService _logger;
+        private readonly IJsonServiceClient _client;
+
+        public AccountController(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ILoggerService logger,
+            IJsonServiceClient client)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _client = client;
+        }
+
+        #region Login
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            if (User.Identity is { IsAuthenticated: true })
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var externalLoginProviders = await _signInManager.GetExternalAuthenticationSchemesAsync();
+
+            ViewData["ExternalLoginProvider"] = externalLoginProviders;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel request)
+        {
+            if (User.Identity is { IsAuthenticated: true })
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(500, "Please enter email or password.");
+            }
+
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user.LockoutEnabled)
+            {
+                return StatusCode(500, $"User with email {request.Email} does not exist or your account got locked. " +
+                                       "Please contact admin for more information");
+            }
+
+            var apiResult = await _client.PostAsync(new AccountModels.GetLogin { Email = request.Email });
+
+            switch (apiResult.StatusCode)
+            {
+                case Common.Enums.StatusCode.Success:
+                    var result = await _signInManager.PasswordSignInAsync(
+                        user,
+                        request.Password,
+                        request.IsPersistent,
+                        true);
+
+                    if (result.IsLockedOut)
+                    {
+                        return StatusCode(500, "User has no permission to access into the system");
+                    }
+
+                    if (!result.Succeeded || result.IsNotAllowed)
+                    {
+                        return StatusCode(500, "Login fail. Found some problems while logging in. You might enter wrong password");
+                    }
+
+                    return Ok();
+                case Common.Enums.StatusCode.InternalServerError:
+                default:
+                    return RedirectToAction("Error", "Home");
+            }
+        }
+
+        #endregion
+
+        #region External Login
+
+        public IActionResult LoginExternally(string request)
+        {
+            _logger.LogInfo("LoginExternally - Start");
+
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(request, null);
+            properties.RedirectUri = Url.Action("ExternalLoginCallback", "Account");
+            _logger.LogInfo($"LoginExternally - {string.Join(", ", properties.Items.Keys)} - " +
+                            $"{string.Join(", ", properties.Items.Values)}");
+
+            _logger.LogInfo("LoginExternally - End");
+
+            return Challenge(properties, request);
+        }
+
+        public async Task<IActionResult> ExternalLoginCallback()
+        {
+            _logger.LogInfo("ExternalLoginCallback - Start");
+            var loginInfo = await _signInManager.GetExternalLoginInfoAsync();
+
+            var externalEmail = loginInfo.Principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+            var externalUsername = loginInfo.Principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+
+            _logger.LogInfo($"Step 1 - ExternalLoginCallback - Email={externalEmail?.Value}");
+
+            if (externalEmail == null) return RedirectToAction("Error", "Home");
+
+            var userExist = await _userManager.FindByEmailAsync(externalEmail.Value);
+
+            if (userExist.LockoutEnabled)
+            {
+                _logger.LogInfo($"Step 1.2 - Account with email [{userExist.Email}] already got lock.");
+                return RedirectToAction("ErrorForbidden", "Account");
+            }
+
+            // External login if account existed
+            var isExternalInfoExist = await _client.GetAsync(new AccountModels.GetUser
+            {
+                Email = externalEmail.Value,
+                ProviderKey = loginInfo.ProviderKey
+            });
+
+            _logger.LogInfo("Step 2 - ExternalLoginCallback");
+
+            if (isExternalInfoExist.StatusCode == Common.Enums.StatusCode.Success)
+            {
+                var user = await _userManager.FindByEmailAsync(externalEmail.Value);
+
+                if (user.LockoutEnabled)
+                {
+                    _logger.LogInfo($"Step 2.2 - Account with email [{user.Email}] already got lock.");
+                    return RedirectToAction("ErrorForbidden", "Account");
+                }
+
+                var isExternalLoginCreated = await _userManager.AddLoginAsync(user, loginInfo);
+
+                if (!isExternalLoginCreated.Succeeded)
+                {
+                    _logger.LogError(
+                        "Error in ExternalLoginCallback request: " +
+                        $"{string.Join(", ", isExternalLoginCreated.Errors.Select(e => e.Description))}");
+                }
+            }
+
+            _logger.LogInfo("Step 3 - ExternalLoginCallback");
+
+            var externalLoginResult = await _client.PostAsync(new AccountModels.GetExternalLogin
+            {
+                ExternalEmail = externalEmail.Value,
+                Username = externalUsername?.Value,
+                LoginProvider = loginInfo.LoginProvider,
+                ProviderDisplayName = loginInfo.ProviderDisplayName,
+                ProviderKey = loginInfo.ProviderKey
+            });
+
+            _logger.LogInfo("Step 4 - ExternalLoginCallback");
+
+            _logger.LogInfo($"ExternalLoginCallback - result = {externalLoginResult.StatusCode}");
+
+            switch (externalLoginResult.StatusCode)
+            {
+                case Common.Enums.StatusCode.Success:
+                    var externalResult = await _signInManager.ExternalLoginSignInAsync(loginInfo.LoginProvider,
+                    loginInfo.ProviderKey, false);
+
+                    if (externalResult.IsLockedOut)
+                    {
+                        return RedirectToAction("ErrorForbidden", "Account");
+                    }
+
+                    if (!externalResult.Succeeded || externalResult.IsNotAllowed)
+                    {
+                        return RedirectToAction("ErrorForbidden", "Account");
+                    }
+
+                    _logger.LogInfo("ExternalLoginCallback - End");
+
+                    return RedirectToAction("Index", "Home", new { loginRequest = "LOGIN" });
+                case Common.Enums.StatusCode.Forbidden:
+                    return RedirectToAction("ErrorForbidden", "Account");
+                case Common.Enums.StatusCode.InternalServerError:
+                default:
+                    return RedirectToAction("ErrorInternalServer", "Account");
+            }
+        }
+
+        #endregion
+
+        #region Register
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Register()
+        {
+            if (User.Identity is { IsAuthenticated: true })
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var externalLoginProviders = await _signInManager.GetExternalAuthenticationSchemesAsync();
+
+            ViewData["ExternalLoginProvider"] = externalLoginProviders;
+            return View();
+        }
+
+        #endregion
+
+        #region Passwords
+
+        public IActionResult ForgotPassword()
         {
             return View();
         }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Error Handler
+        [AllowAnonymous]
+        public IActionResult Denied()
+        {
+            return Content("Denied Request.");
+        }
+
+        public IActionResult ErrorNotFound()
+        {
+            return NotFound("404 Not Found");
+        }
+
+        public IActionResult ErrorForbidden()
+        {
+            return Content("403 Error Forbidden");
+        }
+
+        public IActionResult ErrorInternalServer()
+        {
+            return Content("500 Error InternalServer");
+        }
+        #endregion
     }
 }
