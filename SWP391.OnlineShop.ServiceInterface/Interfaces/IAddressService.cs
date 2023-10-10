6da041a594b20f719d6b35d9f56a20e7b8482d7d
dathmhe141249 <dathmhe141249@fpt.edu.vn>
2023-10-11 00:09:52 +0700

    OSP: Update Function Comment

--- SWP391.OnlineShop.ServiceInterface/Interfaces/IAddressService.cs
+++ SWP391.OnlineShop.ServiceInterface/Interfaces/IAddressService.cs
@@ -6,10 +6,39 @@ namespace SWP391.OnlineShop.ServiceInterface.Interfaces
 {
 	public interface IAddressService
 	{
+		/// <summary>
+		/// Get Address By User Email
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns>Address Of User</returns>
 		Task<AddressViewModel> Get(GetAddressByUser request);
+
+		/// <summary>
+		/// Get District By Province
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns>All District of input Province</returns>
 		List<DistrictViewModel> Get(GetDistrictByProvince request);
+
+		/// <summary>
+		/// Get All Province
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns>List of Province</returns>
 		List<ProvinceViewModel> Get(GetAllProvince request);
+
+		/// <summary>
+		///  Get Ward By District
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns>All Ward of that District</returns>
 		List<WardViewModel> Get(GetWardByDistrict request);
+
+		/// <summary>
+		/// Update Address
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns>API Status Code</returns>
 		Task<BaseResultModel> Put(PutUpdateAddress request);
 	}
 }
