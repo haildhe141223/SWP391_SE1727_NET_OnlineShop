97dae5488e5b244baa8561d9aad36569f7676f29
PhuongNLHE141219 <phuongnlhe141219@fpt.edu.vn>
2023-10-16 00:24:22 +0700

    OSP: Update merge conflict

--- SWP391.OnlineShop.ServiceModel/ServiceModels/ProductModels.cs
+++ SWP391.OnlineShop.ServiceModel/ServiceModels/ProductModels.cs
@@ -5,78 +5,78 @@ using SWP391.OnlineShop.ServiceModel.ViewModels.Products;
 namespace SWP391.OnlineShop.ServiceModel.ServiceModels
 {
 
-	[Route("/Product/GetAllProduct", "GET")]
-	public class GetAllProduct : IReturn<List<ProductViewModel>>
-	{
-	}
+    [Route("/Product/GetAllProduct", "GET")]
+    public class GetAllProduct : IReturn<List<ProductViewModel>>
+    {
+    }
 
-	[Route("/Product/GetProductByCategoryId", "GET")]
-	public class GetProductByCategoryId : IReturn<List<ProductViewModel>>
-	{
-		public int? CategoryId { get; set; }
-	}
+    [Route("/Product/GetProductByCategoryId", "GET")]
+    public class GetProductByCategoryId : IReturn<List<ProductViewModel>>
+    {
+        public int? CategoryId { get; set; }
+    }
 
-	[Route("/Product/GetHotDealProduct", "GET")]
-	public class GetHotDealProduct : IReturn<List<ProductViewModel>>
-	{
-	}
+    [Route("/Product/GetHotDealProduct", "GET")]
+    public class GetHotDealProduct : IReturn<List<ProductViewModel>>
+    {
+    }
 
-	[Route("/Product/GetDealProductOfWeek", "GET")]
-	public class GetDealProductOfWeek : IReturn<List<ProductViewModel>>
-	{
-	}
+    [Route("/Product/GetDealProductOfWeek", "GET")]
+    public class GetDealProductOfWeek : IReturn<List<ProductViewModel>>
+    {
+    }
 
-	[Route("/Product/GetProductById", "GET")]
-	public class GetProductById : IReturn<ProductViewModel>
-	{
-		public int ProductId { get; set; }
-	}
+    [Route("/Product/GetProductById", "GET")]
+    public class GetProductById : IReturn<ProductViewModel>
+    {
+        public int ProductId { get; set; }
+    }
 
-	[Route("/Product/GetProductFeedbackById", "GET")]
-	public class GetProductFeedbackById : IReturn<ProductViewModel>
-	{
-		public int ProductId { get; set; }
-	}
+    [Route("/Product/GetProductFeedbackById", "GET")]
+    public class GetProductFeedbackById : IReturn<ProductViewModel>
+    {
+        public int ProductId { get; set; }
+    }
 
-	[Route("/Product/PostAddProduct", "POST")]
-	public class PostAddProduct : IReturn<ProductViewModel>
-	{
-		public string ProductName { get; set; }
-		public string Thumbnail { get; set; }
-		public string Description { get; set; }
-		public int Amount { get; set; }
-		public decimal Price { get; set; }
-		public decimal SalePrice { get; set; }
-		public int? CategoryId { get; set; }
-	}
+    [Route("/Product/PostAddProduct", "POST")]
+    public class PostAddProduct : IReturn<ProductViewModel>
+    {
+        public string ProductName { get; set; }
+        public string Thumbnail { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal SalePrice { get; set; }
+        public int? CategoryId { get; set; }
+    }
 
-	[Route("/Product/PutUpdateProduct", "PUT")]
-	public class PutUpdateProduct : IReturn<ProductViewModel>
-	{
-		public string ProductName { get; set; }
-    public string Thumbnail { get; set; }
-    public string Description { get; set; }
-    public int Amount { get; set; }
-    public decimal Price { get; set; }
-    public decimal SalePrice { get; set; }
-    public int? CategoryId { get; set; }
-    public int Id { get; set; }
-    public Status Status { get; set; }
-	}
+    [Route("/Product/PutUpdateProduct", "PUT")]
+    public class PutUpdateProduct : IReturn<ProductViewModel>
+    {
+        public string ProductName { get; set; }
+        public string Thumbnail { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal SalePrice { get; set; }
+        public int? CategoryId { get; set; }
+        public int Id { get; set; }
+        public Status Status { get; set; }
+    }
 
-	[Route("/Product/DeleteProduct", "DELETE")]
-	public class DeleteProduct : IReturn<ProductViewModel>
-	{
-		public int ProductId { get; set; }
-	}
+    [Route("/Product/DeleteProduct", "DELETE")]
+    public class DeleteProduct : IReturn<ProductViewModel>
+    {
+        public int ProductId { get; set; }
+    }
 
-	[Route("/Product/Comment", "POST")]
-	public class Comment : IReturnVoid
-	{
-		public string Name { get; set; }
-		public string Email { get; set; }
-		public string Phone { get; set; }
-		public string Message { get; set; }
-		public int ProductID { get; set; }
-	}
+    [Route("/Product/Comment", "POST")]
+    public class Comment : IReturnVoid
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+        public int ProductID { get; set; }
+    }
 }
