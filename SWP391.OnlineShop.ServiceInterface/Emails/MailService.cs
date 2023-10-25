4893c7b3177bb4e3002d15850760e29851f5c995
haildhe141223 <haildhe141223@fpt.edu.vn>
2023-10-26 04:20:41 +0700

    OSP: update Services

---
+++ SWP391.OnlineShop.ServiceInterface/Emails/MailService.cs
@@ -0,0 +1,60 @@
+﻿using SWP391.OnlineShop.Core.Cores.UnitOfWork;
+using SWP391.OnlineShop.Core.Models.Entities;
+using SWP391.OnlineShop.Core.Models.Enums;
+
+namespace SWP391.OnlineShop.ServiceInterface.Emails
+{
+    public class MailService : IMailService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MailService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public int Send(Email email)
+        {
+            email.MailStatus = email.MailStatus == MailStatus.ConditionalPending
+                ? MailStatus.ConditionalPending
+                : MailStatus.New;
+
+            email.ModifiedDateTime = DateTime.Now;
+
+            if (email.Id > 0)
+            {
+                _unitOfWork.Emails.Update(email);
+                _unitOfWork.Complete();
+            }
+            else
+            {
+                _unitOfWork.Emails.Add(email);
+                _unitOfWork.Complete();
+            }
+
+            return email.Id;
+        }
+
+        public async Task<int> SendAsync(Email email)
+        {
+            email.MailStatus = email.MailStatus == MailStatus.ConditionalPending
+                ? MailStatus.ConditionalPending
+                : MailStatus.New;
+
+            email.ModifiedDateTime = DateTime.Now;
+
+            if (email.Id > 0)
+            {
+                _unitOfWork.Emails.Update(email);
+                await _unitOfWork.CompleteAsync();
+            }
+            else
+            {
+                await _unitOfWork.Emails.AddAsync(email);
+                await _unitOfWork.CompleteAsync();
+            }
+
+            return email.Id;
+        }
+    }
+}
---
+++ SWP391.OnlineShop.ServiceModel/ViewModels/Accounts/ForgotPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWP391.OnlineShop.ServiceModel.ViewModels.Accounts;
+
+public class ForgotPasswordViewModel
+{
+    [Required(ErrorMessage = "Email cannot be empty")]
+    [MaxLength(255, ErrorMessage = "Email max-length is 255 characters")]
+    public string Email { get; set; }
+}
