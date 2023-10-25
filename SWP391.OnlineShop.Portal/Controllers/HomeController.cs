ac9c3b168efd933af6b65731f80e3bf7dd9e3b0f
haildhe141223 <haildhe141223@fpt.edu.vn>
2023-10-26 04:20:22 +0700

    OSP: update Login and Register

--- SWP391.OnlineShop.Portal/Controllers/HomeController.cs
+++ SWP391.OnlineShop.Portal/Controllers/HomeController.cs
@@ -8,55 +8,57 @@ using System.Diagnostics;
 
 namespace SWP391.OnlineShop.Portal.Controllers
 {
-	public class HomeController : BaseController
+    public class HomeController : BaseController
     {
-		private readonly IJsonServiceClient _client;
-		private readonly ILoggerService _logger;
+        private readonly IJsonServiceClient _client;
+        private readonly ILoggerService _logger;
 
-		public HomeController(
-			IJsonServiceClient client,
-		ILoggerService logger)
-		{
-			_logger = logger;
-			_client = client;
-		}
+        public HomeController(
+            IJsonServiceClient client,
+        ILoggerService logger)
+        {
+            _logger = logger;
+            _client = client;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			_logger.LogInfo("1. Home Index - Start");
+        public async Task<IActionResult> Index()
+        {
+            _logger.LogInfo("1. Home Index - Start");
 
-			//Get all products
-			var latestProducts = await _client.GetAsync(new GetAllProduct());
+            //Get all products
+            var latestProducts = await _client.GetAsync(new GetAllProduct());
 
-			//Get hot deal product
-			var hotDealProduct = await _client.GetAsync(new GetHotDealProduct());
+            //Get hot deal product
+            var hotDealProduct = await _client.GetAsync(new GetHotDealProduct());
 
-			//Get deal product of week
-			var dealProductOfWeeks = await _client.GetAsync(new GetDealProductOfWeek());
+            //Get deal product of week
+            var dealProductOfWeeks = await _client.GetAsync(new GetDealProductOfWeek());
 
-			//Get all categories
-			var categories = await _client.GetAsync(new GetAllCategory());
+            //Get all categories
+            var categories = await _client.GetAsync(new GetAllCategory());
 
-			var products = new HomeViewModels
-			{
-				LatestProducts = latestProducts,
-				HotDealProduct = hotDealProduct,
-				ProductsOfWeek = dealProductOfWeeks,
-				Categories = categories
-			};
+            var products = new HomeViewModels
+            {
+                LatestProducts = latestProducts,
+                HotDealProduct = hotDealProduct,
+                ProductsOfWeek = dealProductOfWeeks,
+                Categories = categories
+            };
 
-			return View(products);
-		}
+            return View(products);
+        }
 
-		public IActionResult Contact()
-		{
-			return View();
-		}
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var data = new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+            _logger.LogError($"Error - {data.RequestId}");
+            return StatusCode(500, "500: Error with request id. An error occurred while processing your request.");
+        }
+    }
 }
