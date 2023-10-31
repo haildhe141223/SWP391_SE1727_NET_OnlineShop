4d4c28ab4e990c611239e7dc68d31c8b3d2acb86
thuyhe141586 <thuyntthe141586@fpt.edu.vn>
2023-10-31 22:48:06 +0700

    OSP: Update Comment

--- SWP391.OnlineShop.Portal/Controllers/PostController.cs
+++ SWP391.OnlineShop.Portal/Controllers/PostController.cs
@@ -22,30 +22,37 @@ namespace SWP391.OnlineShop.Portal.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            //get all post
             var posts = await _client.GetAsync(new GetAllPost());
             foreach (var post in posts)
             {
+            //get tag by post
                 var tags = await _client.GetAsync(new GetTagByPost()
                 {
                     PostId = post.Id
                 });
                 post.Tags = tags;
             }
+            //get all tag
             ViewData["AllTags"] = await _client.GetAsync(new GetAllTag());
             return View(posts);
         }
 
         public async Task<IActionResult> Details(int id)
         {
+
+        //get post by id
             var post = await _client.GetAsync(new GetPostById()
             {
                 PostId = id
             });
+        //get tag by post
             var tags = await _client.GetAsync(new GetTagByPost()
             {
                 PostId = post.Id
             });
             post.Tags = tags;
+        // get all tag
             ViewData["AllTags"] = await _client.GetAsync(new GetAllTag());
             return View(post);
         }
