78ec727dd0bbfa3580e472b2dadc06b2d75e2506
haildhe141223 <haildhe141223@fpt.edu.vn>
2023-11-02 02:36:17 +0700

    OSP: update user profile

---
+++ SWP391.OnlineShop.Common/Extensions/ValidationExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWP391.OnlineShop.Common.Extensions;
+
+public static class ValidationExtensionMethods
+{
+    public static bool IsEmail(this string email)
+    {
+        var checkEmail = new EmailAddressAttribute();
+        var isValidEmail = checkEmail.IsValid(email);
+        return isValidEmail;
+    }
+}
---
+++ SWP391.OnlineShop.ServiceModel/ViewModels/Profiles/SecurityViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWP391.OnlineShop.ServiceModel.ViewModels.Profiles;
+
+public class SecurityViewModel
+{
+    [Required(ErrorMessage = "Please input current password. This field is required")]
+    public string CurrentPassword { get; set; }
+
+    [Required(ErrorMessage = "Please input new password. This field is required")]
+    public string NewPassword { get; set; }
+
+    [Required(ErrorMessage = "Please input retype password. This field is required")]
+    [StringLength(50, ErrorMessage = "Must be between 8 and 50 characters", MinimumLength = 8)]
+    [Compare(nameof(NewPassword))]
+    public string RePassword { get; set; }
+}
